b9a37b99efc7719eab09011bfb9ba5bd553a5244
FaisalYousuf1 <fyousuf@unomaha.edu>
2024-09-22 22:38:01 -0500

    Update TurtleGraphics.py

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -1,7 +1,7 @@
 #TurtleGraphics.py
-#Name:
-#Date:
-#Assignment:
+#Name:Faisal Yousuf
+#Date:9/22/2024
+#Assignment:Lab 4
 
 import turtle #needed generally but not in CodeHS
 hideturtle() #hides the default turtle in CodeHS
@@ -11,17 +11,19 @@ def drawSquare(myTurtle, size):
         myTurtle.forward(size)
         myTurtle.right(90)
 
+def drawPolygon(bob, sides):
+    for s in range(sides):
+        bob.forward(50)
+        bob.right(360/sides)
+        
 
 def main():
     myTurtle = turtle.Turtle()
+    
+    #drawSquare(myTurtle, 100)
+    
     # drawPolygon(myTurtle, 5) #draws a pentagon
     # drawPolygon(myTurtle, 8) #draws an octogon
-
-    # fillCorner(myTurtle, 2) #draws a square with top right corner filled in.
-    # fillCorner(myTurtle, 3) #draws a square bottom left corner filled in.
-
-    # squaresInSquares(myTurtle, 5) #draws 5 concentric squares
-    # squaresInSquares(myTurtle, 3) #draws 3 concentric squares
-
+    # drawPolygon(myTurtle, 15) #draws an octogon
 
 main()
